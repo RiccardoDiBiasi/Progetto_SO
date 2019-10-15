927acc3d55a8921e9865bbe75b20d8296918888e
Riccardo Di Biasi <dibiasi.1694305@studenti.uniroma1.it>
2019-10-15 23:10:09 +0200

    commit

--- disastrOS_semaphores/disastrOS_semopen.c
+++ disastrOS_semaphores/disastrOS_semopen.c
@@ -6,23 +6,26 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
+#include "disastrOS_globals.h"
+#include "disastrOS_resource.h"
 
-// Creazione del semaforo avente un proprio ID
-// proprio tramite questo ID è possibile accedere al semaforo da tutto il sistema
-// la funzione ritorna il file descriptor(>=0) in caso di successo, un codice di errore(<0) in caso di fallimento
+
+
+// Cerco il semaforo attraverso l'id, se non esiste viene allocato e aggiunto alla lista dei semafori
 
 void internal_semOpen(){
   
     int id_sem = running->syscall_args[0];
     int val_sem = running->syscall_args[1];
 
-    printf("[X] Processo %d ha richiesto l'apertura del semaforo %d\n", running->pid, id_sem);
+    printf("Il processo %d ha richiesto l'apertura del semaforo %d\n", running->pid, id_sem);
 
     //Controllo se non esiste già un semaforo aperto con quell'ID
 
-    Semaphore* s = SemaphoreList_byId(&semaphores_list, id_sem);
+    Semaphore* sem = SemaphoreList_byId(&semaphores_list, id_sem);
+
     if(!sem){
-      sem = Semaphore_alloc(id_sem, val_sem);
+      s = Semaphore_alloc(id_sem, val_sem);
 
       if(!sem){
       running->syscall_retvalue = DSOS_SEMNOTALLOC;
